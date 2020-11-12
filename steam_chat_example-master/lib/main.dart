b35a0dbecffe45d3e1ec422e0539664e5c1ac63a
esme-lopez-r2devpros <esmeralda.lopez@r2devpros.com>
2020-11-11 20:21:00 -0600

    Change

--- steam_chat_example-master/lib/main.dart
+++ steam_chat_example-master/lib/main.dart
@@ -218,7 +218,7 @@ class ChatPage extends StatelessWidget {
       body: Column(
         children: <Widget>[
           Expanded(
-            child: MessageListView(),
+            child: Expanded(child: MessageListView()),
           ),
           MessageInput(),
         ],
